ef1174f2e726915be09cfd67d7a6e6a18e0c6ea9
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2021-07-12 14:02:58 -0400

    created palette

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react'
-
+import Palette from './Palette'
+import seedColors from './seedColors'
 
 class App extends Component {
   render(){
   return (
     <div className="App">
-    <h1>im the app</h1>
+    <Palette {...seedColors[4]}/>
     </div>
   );
 }
---
+++ src/Palette.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+
+class Palette extends Component {
+    constructor(props){
+        super(props)
+
+    }
+    render(){
+        return (
+            <div className="Palette">
+                {/*navbar goes here*/}
+                <div className="Palette-colors">
+                    {/*bunch of color boxes*/}
+                </div>
+                {/*Footer*/}
+            </div>
+        )
+    }
+}
+
+export default Palette;
