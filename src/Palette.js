aa2076dd37ee176f0b8881ef51d0fd75ef68de54
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2021-07-13 09:48:28 -0400

    fixed colorbox css bug and completed colorbox

--- src/Palette.js
+++ src/Palette.js
@@ -1,18 +1,18 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import ColorBox from './ColorBox'
+import './Palette.css'
 
 class Palette extends Component {
-    constructor(props){
-        super(props)
 
-    }
     render(){
+        const colorBoxes = this.props.colors.map(color => (
+            <ColorBox key={color.name} background={color.color} name={color.name}/>
+        ))
         return (
             <div className="Palette">
-                {/*navbar goes here*/}
                 <div className="Palette-colors">
-                    {/*bunch of color boxes*/}
+                    {colorBoxes}
                 </div>
-                {/*Footer*/}
             </div>
         )
     }
