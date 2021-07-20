b170f4dc67007eb4f773636b861edb713a79029b
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2021-07-20 19:59:19 -0400

    created randomize function out of the loop for netlify

--- colors-app/src/NewPaletteForm.js
+++ colors-app/src/NewPaletteForm.js
@@ -55,16 +55,20 @@ class NewPaletteForm extends Component {
     this.setState({ colors: [] });
   }
   addRandomColor() {
-    const allColors = this.props.palettes.map(p => p.colors).flat();
-    let rand;
-    let randomColor;
-    let isDuplicateColor = true;
-    while (isDuplicateColor) {
+    function randomize (){
       rand = Math.floor(Math.random() * allColors.length);
       randomColor = allColors[rand];
       isDuplicateColor = this.state.colors.some(
         color => color.name === randomColor.name
       );
+    }
+
+    const allColors = this.props.palettes.map(p => p.colors).flat();
+    let rand;
+    let randomColor;
+    let isDuplicateColor = true;
+    while (isDuplicateColor) {
+      randomize()
     }
     this.setState({ colors: [...this.state.colors, randomColor] });
   }
