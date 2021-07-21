65e1e8e46d15a23d20c10ae20d116a8a1fc194f1
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2021-07-21 14:42:31 -0400

    update footer styles

--- colors-app/src/styles/PaletteFooterStyles.js
+++ colors-app/src/styles/PaletteFooterStyles.js
@@ -2,7 +2,6 @@ import sizes from "./sizes";
 
 export default {
   PaletteFooter: {
-    padding: "1rem",
     backgroundColor: "#d7f1f3",
     height: "5vh",
     display: "flex",
@@ -15,12 +14,12 @@ export default {
     margin: "0 1rem"
   },
   button: {
-    margin: "1rem .5rem",
-    padding: "1rem 0.5rem",
+    margin: "0 0.5rem",
+    padding: "0 0.5rem",
     textDecoration: "none",
     [sizes.down("xs")]: {
-      margin: "0.2rem",
-      padding: "0.3rem"
+      margin: "0 0.2rem",
+      padding: "0 0.3rem"
     }
   }
 };
